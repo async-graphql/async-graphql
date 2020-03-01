1be04c68e04105631568559ec99633b0875c1964
sunli <scott_s829@163.com>
2020-03-01 21:56:14 +0800

    add some files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+//! # The GraphQL server library implemented by rust
+//!
+//! <div align="center">
+//! <!-- CI -->
+//! <img src="https://github.com/sunli829/potatonet/workflows/CI/badge.svg" />
+//! <!-- Crates version -->
+//! <a href="https://crates.io/crates/async-graphql">
+//! <img src="https://img.shields.io/crates/v/async-graphql.svg?style=flat-square"
+//! alt="Crates.io version" />
+//! </a>
+//! <!-- Downloads -->
+//! <a href="https://crates.io/crates/async-graphql">
+//! <img src="https://img.shields.io/crates/d/async-graphql.svg?style=flat-square"
+//! alt="Download" />
+//! </a>
+//! <!-- docs.rs docs -->
+//! <a href="https://docs.rs/async-graphql">
+//! <img src="https://img.shields.io/badge/docs-latest-blue.svg?style=flat-square"
+//! alt="docs.rs docs" />
+//! </a>
+//! </div>
+//!
+//! ## Documentation
+//!
+//! * [GitHub repository](https://github.com/sunli829/async-graphql)
+//! * [Cargo package](https://crates.io/crates/async-graphql)
+//! * Minimum supported Rust version: 1.39 or later
+//!
+//! ## References
+//!
+//! * [GraphQL](https://graphql.org)
+
 #[macro_use]
 extern crate thiserror;
 #[macro_use]
