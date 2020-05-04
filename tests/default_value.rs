0d32eaac757c900bb444b2e50650da72df348679
sunli <scott_s829@163.com>
2020-05-04 13:26:13 +0800

    Update default_value.rs

--- tests/default_value.rs
+++ tests/default_value.rs
@@ -1,7 +1,7 @@
 use async_graphql::*;
 
 #[async_std::test]
-pub async fn test_interface_simple_object() {
+pub async fn test_default_value_arg() {
     struct Query;
 
     #[Object]
@@ -21,6 +21,42 @@ pub async fn test_interface_simple_object() {
     );
 
     let query = "{ value(input: 88) }";
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    assert_eq!(
+        schema.execute(&query).await.unwrap().data,
+        serde_json::json!({
+            "value": 88
+        })
+    );
+}
+
+#[async_std::test]
+pub async fn test_default_value_inputobject() {
+    #[InputObject]
+    struct MyInput {
+        #[field(default = "100")]
+        value: i32,
+    }
+
+    struct Query;
+
+    #[Object]
+    impl Query {
+        async fn value(&self, input: MyInput) -> i32 {
+            input.value
+        }
+    }
+
+    let query = "{ value(input: {}) }";
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    assert_eq!(
+        schema.execute(&query).await.unwrap().data,
+        serde_json::json!({
+            "value": 100
+        })
+    );
+
+    let query = "{ value(input: { value: 88 }) }";
     let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
     assert_eq!(
         schema.execute(&query).await.unwrap().data,
