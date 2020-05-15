ac2fe8583c2ccb16437b8e948255501359f79723
Caio <c410.f3r@gmail.com>
2020-05-15 08:51:54 -0300

    Impl Scalar for NaiveDate and NaiveTime

---
+++ src/scalars/naive_time.rs
@@ -0,0 +1,22 @@
+use crate::{InputValueError, InputValueResult, Result, ScalarType, Value};
+use async_graphql_derive::Scalar;
+use chrono::NaiveTime;
+
+/// Implement the NaiveTime scalar
+#[Scalar(internal)]
+impl ScalarType for NaiveTime {
+    fn type_name() -> &'static str {
+        "NaiveTime"
+    }
+
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::String(s) => Ok(NaiveTime::parse_from_str(&s, "%H:%M:%S")?),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn to_json(&self) -> Result<serde_json::Value> {
+        Ok(self.format("%H:%M:%S").to_string().into())
+    }
+}
