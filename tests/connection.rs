f74969fbc3d81ad0c4e107116c34a6ee974be419
Sunli <scott_s829@163.com>
2020-05-27 14:21:00 +0800

    Add test for additional fields

--- tests/connection.rs
+++ tests/connection.rs
@@ -115,3 +115,117 @@ pub async fn test_slice_datasource() {
     )
     .await;
 }
+
+#[async_std::test]
+pub async fn test_datasource_additional_fields() {
+    struct QueryRoot;
+
+    struct Numbers;
+
+    #[SimpleObject]
+    struct ConnectionFields {
+        total_count: i32,
+    }
+
+    #[SimpleObject]
+    struct Diff {
+        diff: i32,
+    }
+
+    #[DataSource]
+    impl DataSource for Numbers {
+        type CursorType = usize;
+        type ElementType = i32;
+        type ConnectionFieldsType = ConnectionFields;
+        type EdgeFieldsType = Diff;
+
+        async fn execute_query(
+            &self,
+            _ctx: &Context<'_>,
+            after: Option<usize>,
+            before: Option<usize>,
+            first: Option<usize>,
+            last: Option<usize>,
+        ) -> FieldResult<
+            Connection<
+                Self::CursorType,
+                Self::ElementType,
+                Self::ConnectionFieldsType,
+                Self::EdgeFieldsType,
+            >,
+        > {
+            let mut start = after.map(|after| after + 1).unwrap_or(0);
+            let mut end = before.unwrap_or(10000);
+            if let Some(first) = first {
+                end = (start + first).min(end);
+            }
+            if let Some(last) = last {
+                start = if last > end - start { end } else { end - last };
+            }
+            let mut connection = Connection::new_with_additional_fields(
+                start > 0,
+                end < 10000,
+                ConnectionFields { total_count: 10000 },
+            );
+            connection.append((start..end).into_iter().map(|n| {
+                Ok(Edge::new_with_additional_fields(
+                    n,
+                    n as i32,
+                    Diff {
+                        diff: (10000 - n) as i32,
+                    },
+                ))
+            }))?;
+            Ok(connection)
+        }
+    }
+
+    #[Object]
+    impl QueryRoot {
+        async fn numbers(
+            &self,
+            ctx: &Context<'_>,
+            after: Option<String>,
+            before: Option<String>,
+            first: Option<i32>,
+            last: Option<i32>,
+        ) -> FieldResult<Connection<usize, i32, ConnectionFields, Diff>> {
+            Numbers.query(ctx, after, before, first, last).await
+        }
+    }
+
+    let schema = Schema::new(QueryRoot, EmptyMutation, EmptySubscription);
+
+    assert_eq!(
+        schema
+            .execute("{ numbers(first: 2) { totalCount edges { node diff } } }")
+            .await
+            .unwrap()
+            .data,
+        serde_json::json!({
+            "numbers": {
+                "totalCount": 10000,
+                "edges": [
+                    {"node": 0, "diff": 10000},
+                    {"node": 1, "diff": 9999},
+                ]
+            },
+        })
+    );
+
+    assert_eq!(
+        schema
+            .execute("{ numbers(last: 2) { edges { node diff } } }")
+            .await
+            .unwrap()
+            .data,
+        serde_json::json!({
+            "numbers": {
+                "edges": [
+                    {"node": 9998, "diff": 2},
+                    {"node": 9999, "diff": 1},
+                ]
+            },
+        })
+    );
+}
