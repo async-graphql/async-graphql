dfe381b2e0740daa0a5d682a10e4a5cb4c1f88ab
Sunli <scott_s829@163.com>
2020-05-28 10:26:07 +0800

    Fix the problem that async-graphql cannot be compiled and passed in rust nightly-2020-05-25. #122

--- src/scalars/floats.rs
+++ src/scalars/floats.rs
@@ -1,33 +1,48 @@
 use crate::{InputValueError, InputValueResult, ScalarType, Value};
 use async_graphql_derive::Scalar;
 
-macro_rules! impl_float_scalars {
-    ($($ty:ty),*) => {
-        $(
-        /// The `Float` scalar type represents signed double-precision fractional values as specified by [IEEE 754](https://en.wikipedia.org/wiki/IEEE_floating_point).
-        #[Scalar(internal, name = "Float")]
-        impl ScalarType for $ty {
-            fn parse(value: Value) -> InputValueResult<Self> {
-                match value {
-                    Value::Int(n) => Ok(n as Self),
-                    Value::Float(n) => Ok(n as Self),
-                    _ => Err(InputValueError::ExpectedType(value))
-                }
-            }
-
-            fn is_valid(value: &Value) -> bool {
-                match value {
-                    Value::Int(_) | Value::Float(_) => true,
-                    _ => false
-                }
-            }
-
-            fn to_value(&self) -> Value {
-                Value::Float(*self as f64)
-            }
+/// The `Float` scalar type represents signed double-precision fractional values as specified by [IEEE 754](https://en.wikipedia.org/wiki/IEEE_floating_point).
+#[Scalar(internal, name = "Float")]
+impl ScalarType for f32 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            Value::Float(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
         }
-        )*
-    };
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) | Value::Float(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Float(*self as f64)
+    }
 }
 
-impl_float_scalars!(f32, f64);
+/// The `Float` scalar type represents signed double-precision fractional values as specified by [IEEE 754](https://en.wikipedia.org/wiki/IEEE_floating_point).
+#[Scalar(internal, name = "Float")]
+impl ScalarType for f64 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            Value::Float(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) | Value::Float(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Float(*self as f64)
+    }
+}
--- src/scalars/integers.rs
+++ src/scalars/integers.rs
@@ -1,63 +1,180 @@
 use crate::{InputValueError, InputValueResult, ScalarType, Value};
 use async_graphql_derive::Scalar;
 
-macro_rules! impl_integer_scalars {
-    ($($ty:ty),*) => {
-        $(
-        /// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
-        #[Scalar(internal, name = "Int")]
-        impl ScalarType for $ty {
-            fn parse(value: Value) -> InputValueResult<Self> {
-                match value {
-                    Value::Int(n) => Ok(n as Self),
-                    _ => Err(InputValueError::ExpectedType(value))
-                }
-            }
-
-            fn is_valid(value: &Value) -> bool {
-                match value {
-                    Value::Int(_) => true,
-                    _ => false
-                }
-            }
-
-            fn to_value(&self) -> Value {
-                Value::Int(*self as i32)
-            }
+/// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+#[Scalar(internal, name = "Int")]
+impl ScalarType for i8 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
         }
-        )*
-    };
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Int(*self as i32)
+    }
 }
 
-impl_integer_scalars!(i8, i16, i32, u8, u16, u32);
-
-macro_rules! impl_int64_scalars {
-    ($($ty:ty),*) => {
-        $(
-        /// The `Int64` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^64) and 2^64 - 1.
-        #[Scalar(internal, name = "Int64")]
-        impl ScalarType for $ty {
-            fn parse(value: Value) -> InputValueResult<Self> {
-                match value {
-                    Value::Int(n) => Ok(n as Self),
-                    Value::String(s) => Ok(s.parse()?),
-                    _ => Err(InputValueError::ExpectedType(value))
-                }
-            }
-
-            fn is_valid(value: &Value) -> bool {
-                match value {
-                    Value::Int(_) | Value::String(_) => true,
-                    _ => false
-                }
-            }
-
-            fn to_value(&self) -> Value {
-                Value::String(self.to_string())
-            }
+/// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+#[Scalar(internal, name = "Int")]
+impl ScalarType for i16 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
         }
-        )*
-    };
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Int(*self as i32)
+    }
 }
 
-impl_int64_scalars!(i64, u64);
+/// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+#[Scalar(internal, name = "Int")]
+impl ScalarType for i32 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Int(*self as i32)
+    }
+}
+
+/// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+#[Scalar(internal, name = "Int")]
+impl ScalarType for u8 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Int(*self as i32)
+    }
+}
+
+/// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+#[Scalar(internal, name = "Int")]
+impl ScalarType for u16 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Int(*self as i32)
+    }
+}
+
+/// The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+#[Scalar(internal, name = "Int")]
+impl ScalarType for u32 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Int(*self as i32)
+    }
+}
+
+/// The `Int64` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^64) and 2^64 - 1.
+#[Scalar(internal, name = "Int64")]
+impl ScalarType for i64 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            Value::String(s) => Ok(s.parse()?),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) | Value::String(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::String(self.to_string())
+    }
+}
+
+/// The `Int64` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^64) and 2^64 - 1.
+#[Scalar(internal, name = "Int64")]
+impl ScalarType for u64 {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        match value {
+            Value::Int(n) => Ok(n as Self),
+            Value::String(s) => Ok(s.parse()?),
+            _ => Err(InputValueError::ExpectedType(value)),
+        }
+    }
+
+    fn is_valid(value: &Value) -> bool {
+        match value {
+            Value::Int(_) | Value::String(_) => true,
+            _ => false,
+        }
+    }
+
+    fn to_value(&self) -> Value {
+        Value::String(self.to_string())
+    }
+}
