eb4d70b50ec2629a4a6b2bcbeac384abb5c86384
Sunli <scott_s829@163.com>
2020-05-28 14:13:22 +0800

    Update maybe_undefined.rs

--- src/types/maybe_undefined.rs
+++ src/types/maybe_undefined.rs
@@ -4,10 +4,51 @@ use std::borrow::Cow;
 /// Similar to `Option`, but it has three states, `undefined`, `null` and `x`.
 ///
 /// Spec: https://spec.graphql.org/June2018/#sec-Null-Value
+#[allow(missing_docs)]
 pub enum MaybeUndefined<T> {
     Undefined,
     Null,
     Value(T),
+}
+
+impl<T> MaybeUndefined<T> {
+    /// Returns true if the MaybeUndefined<T> is undefined.
+    #[inline]
+    pub fn is_undefined(&self) -> bool {
+        if let MaybeUndefined::Undefined = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Returns true if the MaybeUndefined<T> is null.
+    #[inline]
+    pub fn is_null(&self) -> bool {
+        if let MaybeUndefined::Null = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Borrow the value, returns `None` if the value is `undefined` or `null`, otherwise returns `Some(T)`.
+    #[inline]
+    pub fn value(&self) -> Option<&T> {
+        match self {
+            MaybeUndefined::Value(value) => Some(value),
+            _ => None,
+        }
+    }
+
+    /// Convert MaybeUndefined<T> to Option<T>.
+    #[inline]
+    pub fn take(self) -> Option<T> {
+        match self {
+            MaybeUndefined::Value(value) => Some(value),
+            _ => None,
+        }
+    }
 }
 
 impl<T: Type> Type for MaybeUndefined<T> {
@@ -30,7 +71,7 @@ impl<T: InputValueType> InputValueType for MaybeUndefined<T> {
         match value {
             None => Ok(MaybeUndefined::Undefined),
             Some(Value::Null) => Ok(MaybeUndefined::Null),
-            Some(value) => Ok(MaybeUndefined::Value(Option::<T>::parse(Some(value))?)),
+            Some(value) => Ok(MaybeUndefined::Value(T::parse(Some(value))?)),
         }
     }
 
