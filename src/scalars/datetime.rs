0a0fc03e8c487cda92f00fdd712aea89a9238127
Jake Thompson <jake@zenlist.com>
2020-06-16 01:46:20 +0000

    Update parsing to support more formats, this includes the JS standard format that is not compatible with the `%+` time format

--- src/scalars/datetime.rs
+++ src/scalars/datetime.rs
@@ -1,6 +1,6 @@
 use crate::{InputValueError, InputValueResult, ScalarType, Value};
 use async_graphql_derive::Scalar;
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Utc};
 
 /// Implement the DateTime<Utc> scalar
 ///
@@ -8,8 +8,10 @@ use chrono::{DateTime, TimeZone, Utc};
 #[Scalar(internal, name = "DateTimeUtc")]
 impl ScalarType for DateTime<Utc> {
     fn parse(value: Value) -> InputValueResult<Self> {
-        match value {
-            Value::String(s) => Ok(Utc.datetime_from_str(&s, "%+")?),
+        match &value {
+            Value::String(s) => s
+                .parse::<DateTime<Utc>>()
+                .map_err(|_| InputValueError::ExpectedType(value)),
             _ => Err(InputValueError::ExpectedType(value)),
         }
     }
