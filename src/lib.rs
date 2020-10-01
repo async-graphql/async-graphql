f33ab0af0197f7ef0b76004b5ca7436c37c904c4
Sunli <scott_s829@163.com>
2020-10-01 10:05:16 +0800

    Inline ContainerType, EnumType, ScalarType to docs.

--- src/lib.rs
+++ src/lib.rs
@@ -169,14 +169,13 @@ pub use look_ahead::Lookahead;
 pub use parser::types::{ConstValue as Value, Number};
 pub use registry::CacheControl;
 pub use request::{BatchRequest, Request};
+pub use resolver_utils::{ContainerType, EnumType, ScalarType};
 pub use response::{BatchResponse, Response};
 pub use schema::{Schema, SchemaBuilder, SchemaEnv};
 pub use validation::ValidationMode;
 
 #[doc(no_inline)]
 pub use parser::{Pos, Positioned};
-#[doc(no_inline)]
-pub use resolver_utils::{ContainerType, EnumType, ScalarType};
 pub use types::*;
 
 /// Define a GraphQL object with methods
