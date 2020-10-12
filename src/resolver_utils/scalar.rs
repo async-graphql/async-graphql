8b1ddcdc0b239ce56b54927672a8ee1c818a0bac
Sunli <scott_s829@163.com>
2020-10-12 20:40:55 +0800

    Add scalar macro.

--- src/resolver_utils/scalar.rs
+++ src/resolver_utils/scalar.rs
@@ -41,3 +41,117 @@ pub trait ScalarType: Sized + Send {
     /// Convert the scalar to `Value`.
     fn to_value(&self) -> Value;
 }
+
+/// Define a scalar
+///
+/// If your type implemented `serde::Serialize` and `serde::Deserialize`, then you can use this macro to define a scalar more simply.
+/// It helps you implement the `ScalarType::parse` and `ScalarType::to_value` functions by calling the [from_value](fn.from_value.html) and [to_value](fn.to_value.html) functions.
+///
+/// # Examples
+///
+/// ```rust
+/// use async_graphql::*;
+/// use serde::{Serialize, Deserialize};
+/// use std::collections::HashMap;
+///
+/// #[derive(Serialize, Deserialize)]
+/// struct MyValue {
+///     a: i32,
+///     b: HashMap<String, i32>,     
+/// }
+///
+/// scalar!(MyValue);
+///
+/// // Rename to `MV`.
+/// // scalar!(MyValue, "MV");
+///
+/// // Rename to `MV` and add description.
+/// // scalar!(MyValue, "MV", "This is my value");
+///
+/// struct Query;
+///
+/// #[Object]
+/// impl Query {
+///     async fn value(&self, input: MyValue) -> MyValue {
+///         input
+///     }
+/// }
+///
+/// async_std::task::block_on(async move {
+///     let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+///     let res = schema.execute(r#"{ value(input: {a: 10, b: {v1: 1, v2: 2} }) }"#).await.into_result().unwrap().data;
+///     assert_eq!(res, value!({
+///         "value": {
+///             "a": 10,
+///             "b": {"v1": 1, "v2": 2},
+///         }
+///     }));
+/// });
+///
+///
+/// ```
+#[macro_export]
+macro_rules! scalar {
+    ($ty:ty, $name:expr, $desc:literal) => {
+        scalar_internal!($ty, stringify!($ty), Some($desc));
+    };
+
+    ($ty:ty, $name:expr) => {
+        scalar_internal!($ty, stringify!($ty), None);
+    };
+
+    ($ty:ty) => {
+        scalar_internal!($ty, stringify!($ty), None);
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! scalar_internal {
+    ($ty:ty, $name:expr, $desc:expr) => {
+        impl $crate::Type for $ty {
+            fn type_name() -> ::std::borrow::Cow<'static, str> {
+                ::std::borrow::Cow::Borrowed($name)
+            }
+
+            fn create_type_info(registry: &mut $crate::registry::Registry) -> String {
+                registry.create_type::<$ty, _>(|_| $crate::registry::MetaType::Scalar {
+                    name: $name.into(),
+                    description: $desc,
+                    is_valid: |value| <$ty as $crate::ScalarType>::is_valid(value),
+                })
+            }
+        }
+
+        impl $crate::ScalarType for $ty {
+            fn parse(value: $crate::Value) -> $crate::InputValueResult<Self> {
+                Ok($crate::from_value(value)?)
+            }
+
+            fn to_value(&self) -> $crate::Value {
+                $crate::to_value(self).unwrap_or_else(|_| $crate::Value::Null)
+            }
+        }
+
+        impl $crate::InputValueType for $ty {
+            fn parse(value: Option<$crate::Value>) -> $crate::InputValueResult<Self> {
+                <$ty as $crate::ScalarType>::parse(value.unwrap_or_default())
+            }
+
+            fn to_value(&self) -> $crate::Value {
+                <$ty as $crate::ScalarType>::to_value(self)
+            }
+        }
+
+        #[$crate::async_trait::async_trait]
+        impl $crate::OutputValueType for $ty {
+            async fn resolve(
+                &self,
+                _: &$crate::ContextSelectionSet<'_>,
+                _field: &$crate::Positioned<$crate::parser::types::Field>,
+            ) -> $crate::ServerResult<$crate::Value> {
+                Ok($crate::ScalarType::to_value(self))
+            }
+        }
+    };
+}
