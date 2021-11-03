f4c8222ef88fad1f453d4f2e8218d5f835b380d1
meh <meh@schizofreni.co>
2021-11-03 14:56:42 +0100

    docs: add specified_by_url for Duration
    Co-authored-by: Anthony Griffon <an.griffon@gmail.com>

--- src/types/external/duration.rs
+++ src/types/external/duration.rs
@@ -6,7 +6,7 @@ use crate::{InputValueError, InputValueResult, Scalar, ScalarType, Value};
 /// Implement the Duration scalar
 ///
 /// The input/output is a string in ISO8601 format.
-#[Scalar(internal, name = "Duration")]
+#[Scalar(internal, name = "Duration", specified_by_url = "https://en.wikipedia.org/wiki/ISO_8601#Durations")]
 impl ScalarType for Duration {
     fn parse(value: Value) -> InputValueResult<Self> {
         match &value {
