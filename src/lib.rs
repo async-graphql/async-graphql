e4b1f721aab32feb1fcd4df0b8c440042e7eb565
Douman <douman@gmx.se>
2022-05-27 12:31:10 +0900

    Expose IntrospectionMode

--- src/lib.rs
+++ src/lib.rs
@@ -238,7 +238,7 @@ pub use request::{BatchRequest, Request};
 #[doc(no_inline)]
 pub use resolver_utils::{ContainerType, EnumType, ScalarType};
 pub use response::{BatchResponse, Response};
-pub use schema::{Schema, SchemaBuilder, SchemaEnv};
+pub use schema::{IntrospectionMode, Schema, SchemaBuilder, SchemaEnv};
 #[doc(hidden)]
 pub use static_assertions;
 pub use subscription::SubscriptionType;
