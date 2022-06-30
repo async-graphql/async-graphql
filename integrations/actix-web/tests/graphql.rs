6357d83ef5b4951e2a3d0ce5b92aa4b3760ce797
Edward Rudd <urkle@outoforder.cc>
2022-06-29 23:15:55 -0400

    fix format of other code due to new nightly

--- integrations/actix-web/tests/graphql.rs
+++ integrations/actix-web/tests/graphql.rs
@@ -137,13 +137,11 @@ async fn test_hello_header() {
 async fn test_count() {
     let srv = test::init_service(
         App::new()
-            .app_data(
-                Data::new(
-                    Schema::build(CountQueryRoot, CountMutation, EmptySubscription)
-                        .data(Count::default())
-                        .finish(),
-                ),
-            )
+            .app_data(Data::new(
+                Schema::build(CountQueryRoot, CountMutation, EmptySubscription)
+                    .data(Count::default())
+                    .finish(),
+            ))
             .service(
                 web::resource("/")
                     .guard(guard::Post())
