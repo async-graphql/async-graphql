6bd00192cb7e0d9b790129f788f757718a284b4d
Brice Johnson <johnson.brice.a@gmail.com>
2023-08-12 15:32:10 -0700

    Allow Uuids to be used as cursor

--- src/types/connection/cursor.rs
+++ src/types/connection/cursor.rs
@@ -116,6 +116,19 @@ impl CursorType for ID {
     }
 }
 
+#[cfg(feature = "uuid")]
+impl CursorType for uuid::Uuid {
+    type Error = uuid::Error;
+
+    fn decode_cursor(s: &str) -> Result<Self, Self::Error> {
+        s.parse()
+    }
+
+    fn encode_cursor(&self) -> String {
+        self.to_string()
+    }
+}
+
 /// A opaque cursor that encode/decode the value to base64
 pub struct OpaqueCursor<T>(pub T);
 
