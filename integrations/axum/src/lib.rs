a59b58812ad9b3fa5f383b3f1d146c153190fe06
Alexandre Faria <alexandre.faria@auroka.pt>
2024-04-30 22:17:30 +0100

    Added partial support for wasm - disabling subscriptions for wasm.

--- integrations/axum/src/lib.rs
+++ integrations/axum/src/lib.rs
@@ -6,9 +6,11 @@
 mod extract;
 mod query;
 mod response;
+#[cfg(not(target_arch = "wasm32"))]
 mod subscription;
 
 pub use extract::{GraphQLBatchRequest, GraphQLRequest};
 pub use query::GraphQL;
 pub use response::GraphQLResponse;
+#[cfg(not(target_arch = "wasm32"))]
 pub use subscription::{GraphQLProtocol, GraphQLSubscription, GraphQLWebSocket};
