a86441f92fffe0c2a92e619b16ddf5bfddceb0e6
PookieBuns <allenyuchen@hotmail.com>
2024-06-13 01:03:33 -0700

    fix: Use field_name to allow rename

--- derive/src/complex_object.rs
+++ derive/src/complex_object.rs
@@ -171,7 +171,7 @@ pub fn generate(
             });
             if object_args.interface {
                 method.sig.ident = Ident::new(
-                    &format!("interface_impl_{}", method.sig.ident),
+                    &format!("interface_impl_{}", field_name),
                     method.sig.ident.span(),
                 );
             }
