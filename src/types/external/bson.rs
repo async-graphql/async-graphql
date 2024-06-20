3c1ac1f6eb04adf3104728dcc8496f4b2a5fd403
Lukas Norkowski <lukasnorkowski@yahoo.de>
2024-06-20 19:03:25 +0200

    Use bson::extjson instead of manual parsing the async_graphql::Value

--- src/types/external/bson.rs
+++ src/types/external/bson.rs
@@ -5,26 +5,18 @@ use bson::{oid::ObjectId, Bson, Document, Uuid};
 use chrono::{DateTime, Utc};
 
 use crate::{InputValueError, InputValueResult, Scalar, ScalarType, Value};
-use base64::Engine;
 
 #[Scalar(internal)]
 impl ScalarType for ObjectId {
     fn parse(value: Value) -> InputValueResult<Self> {
         match value {
             Value::String(s) => Ok(ObjectId::parse_str(s)?),
-            Value::Object(mut o) => {
-                if o.len() > 1 {
-                    return Err(InputValueError::custom(
-                        "too many keys to be extended json representation of a BSON ObjectId",
-                    ));
-                }
-                let Some(v) = o.shift_remove("$oid") else {
-                    return Err(InputValueError::custom("missing field \"$oid\""));
-                };
-                let Value::String(s) = v else {
-                    return Err(InputValueError::expected_type(v));
-                };
-                Ok(ObjectId::parse_str(s)?)
+            Value::Object(o) => {
+                let json = Value::Object(o).into_json()?;
+                let bson = Bson::try_from(json)?;
+                bson.as_object_id().ok_or(InputValueError::custom(
+                    "could not parse the value as a BSON ObjectId",
+                ))
             }
             _ => Err(InputValueError::expected_type(value)),
         }
@@ -40,59 +32,16 @@ impl ScalarType for Uuid {
     fn parse(value: Value) -> InputValueResult<Self> {
         match value {
             Value::String(s) => Ok(Uuid::parse_str(s)?),
-            Value::Object(mut o) => {
-                if o.len() > 1 {
+            Value::Object(o) => {
+                let json = Value::Object(o).into_json()?;
+                let Bson::Binary(binary) = Bson::try_from(json)? else {
                     return Err(InputValueError::custom(
-                        "too many keys to be extended json representation of a BSON Uuid",
-                    ));
-                }
-                let Some(v) = o.shift_remove("$binary") else {
-                    return Err(InputValueError::custom("missing field \"$binary\""));
-                };
-                let Value::Object(mut o) = v else {
-                    return Err(InputValueError::expected_type(v));
-                };
-                if o.len() > 2 {
-                    return Err(InputValueError::custom(
-                        "too many keys to be extended json representation of a BSON Uuid",
-                    ));
-                }
-                let Some(t) = o.shift_remove("subType") else {
-                    return Err(InputValueError::custom("missing field \"subType\""));
-                };
-                let Value::String(t) = t else {
-                    return Err(InputValueError::expected_type(t));
-                };
-                let Ok(t) = u8::from_str_radix(&t, 16) else {
-                    return Err(InputValueError::custom(
-                        "could not decode subType from hex string",
+                        "could not parse the value as BSON Binary",
                     ));
                 };
-                if t != <bson::spec::BinarySubtype as Into<u8>>::into(
-                    bson::spec::BinarySubtype::Uuid,
-                ) || t
-                    != <bson::spec::BinarySubtype as Into<u8>>::into(
-                        bson::spec::BinarySubtype::UuidOld,
-                    )
-                {
-                    return Err(InputValueError::custom(
-                        "wrong BSON binary subtype to be a BSON Uuid",
-                    ));
-                }
-                let Some(payload) = o.shift_remove("base64") else {
-                    return Err(InputValueError::custom("missing field \"base64\""));
-                };
-                let Value::String(s) = payload else {
-                    return Err(InputValueError::expected_type(payload));
-                };
-                let Ok(payload_bytes) = base64::prelude::BASE64_STANDARD.decode(&s) else {
-                    return Err(InputValueError::custom("could not decode payload"));
-                };
-                let Ok(payload_bytes) = <Vec<u8> as TryInto<[u8; 16]>>::try_into(payload_bytes)
-                else {
-                    return Err(InputValueError::custom("wrong number of payload bytes"));
-                };
-                Ok(Uuid::from_bytes(payload_bytes))
+                binary.to_uuid().map_err(|_| {
+                    InputValueError::custom("could not deserialize BSON Binary to Uuid")
+                })
             }
             _ => Err(InputValueError::expected_type(value)),
         }
