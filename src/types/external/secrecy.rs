14f79c376dd433a4e123f7f64f817c83d4f6c364
Christopher Edward Dawn <cedtwo@tutanota.com>
2024-11-07 14:30:20 +0800

    Add support for secrecy::SecretString

--- src/types/external/secrecy.rs
+++ src/types/external/secrecy.rs
@@ -34,3 +34,33 @@ impl<T: InputType + Zeroize> InputType for SecretBox<T> {
         self.expose_secret().as_raw_value()
     }
 }
+
+impl InputType for SecretString {
+    type RawValueType = str;
+
+    fn type_name() -> Cow<'static, str> {
+        String::type_name()
+    }
+
+    fn qualified_type_name() -> String {
+        String::qualified_type_name()
+    }
+
+    fn create_type_info(registry: &mut registry::Registry) -> String {
+        String::create_type_info(registry)
+    }
+
+    fn parse(value: Option<Value>) -> InputValueResult<Self> {
+        String::parse(value)
+            .map(SecretString::from)
+            .map_err(InputValueError::propagate)
+    }
+
+    fn to_value(&self) -> Value {
+        Value::Null
+    }
+
+    fn as_raw_value(&self) -> Option<&Self::RawValueType> {
+        Some(self.expose_secret())
+    }
+}
