648147e1b7a415b71fcd411ed4bee6caff80ea55
Negezor <negezor@gmail.com>
2025-02-12 22:38:30 +1100

    chore(validators): update zxcvbn in min_password_strength validator

--- src/validators/min_password_strength.rs
+++ src/validators/min_password_strength.rs
@@ -1,4 +1,4 @@
-use zxcvbn::{zxcvbn, ZxcvbnError};
+use zxcvbn::{zxcvbn, Score};
 
 use crate::{InputType, InputValueError};
 
@@ -6,16 +6,20 @@ pub fn min_password_strength<T: AsRef<str> + InputType>(
     value: &T,
     min_score: u8,
 ) -> Result<(), InputValueError<T>> {
-    match zxcvbn(value.as_ref(), &[]) {
-        Ok(password_strength) => {
-            if password_strength.score() < min_score {
-                Err("password is too weak".into())
-            } else {
-                Ok(())
-            }
-        }
-        Err(ZxcvbnError::BlankPassword) => Err("password is too weak".into()),
-        _ => Err("error processing password strength".into()),
+    let min_score = match min_score {
+        0 => Score::Zero,
+        1 => Score::One,
+        2 => Score::Two,
+        3 => Score::Three,
+        _ => Score::Four,
+    };
+
+    let score = zxcvbn(value.as_ref(), &[]).score();
+
+    if score < min_score {
+        Err("password is too weak".into())
+    } else {
+        Ok(())
     }
 }
 
