98cda87bd5eb25593ee50e4d66820c62fcd6414e
Aumetra Weisman <aumetra@cryptolab.net>
2026-01-22 17:35:00 +0100

    Factor out OpenTelemetry into extras crate (#1792)
    * Factor out OpenTelemetry into extras crate
    
    * rustfmt
    
    * Change structure
    
    * Bump commit

---
+++ extras/src/lib.rs
@@ -0,0 +1,10 @@
+//! Extra integrations for async-graphql
+
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
+#![allow(clippy::cast_possible_wrap)]
+#![warn(missing_docs)]
+#![forbid(unsafe_code)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+pub mod extensions;
